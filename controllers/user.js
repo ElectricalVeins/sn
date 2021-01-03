c8ac3429f92b4a5b75a41d39984dde0e439fd00e
Vladimir <maxmost512@gmail.com>
2021-01-03 17:34:24 +0200

    Fix: exclude password from server response

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,7 @@ module.exports.create = async (req, res, next) => {
   try {
     const user = await User.create(body);
     if (user) {
+      delete user.passwordHash;
       return res.send({ data: user });
     }
     throw new Error('400'); //Заглушка пока нет обработчика
@@ -15,7 +16,11 @@ module.exports.create = async (req, res, next) => {
 
 module.exports.getMany = async (req, res, next) => {
   try {
-    const { count, rows } = await User.findAndCountAll();
+    const { count, rows } = await User.findAndCountAll({
+      attributes: {
+        exclude: ['passwordHash'],
+      },
+    });
     res.send({
       meta: { count },
       data: rows,
@@ -30,7 +35,11 @@ module.exports.getById = async (req, res, next) => {
     query: { userId },
   } = req;
   try {
-    const user = await User.findByPk(userId);
+    const user = await User.findByPk(userId, {
+      attributes: {
+        exclude: ['passwordHash'],
+      },
+    });
     if (user) {
       res.send({ data: user });
     }
