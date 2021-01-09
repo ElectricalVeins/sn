9cdaac5308859ff4c048442b8a0faa02723e9fbc
Vladimir <maxmost512@gmail.com>
2021-01-09 21:23:28 +0200

    Connect authRouter

--- app/router.js
+++ app/router.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const userRouter = require('../routes/users');
+const authRouter = require('../routes/auth');
 
 const router = Router();
 
+router.use('/auth', authRouter);
 router.use('/users', userRouter);
 
 module.exports = router;
