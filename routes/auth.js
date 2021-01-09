5e95d29ad0d9aef7ec7bebff40ddebb6586fd797
Vladimir <maxmost512@gmail.com>
2021-01-09 21:23:11 +0200

    Add authRouter

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const AuthController = require('../controllers/auth');
+const { validateSignIn, validateSignUp } = require('../middlewares/validation');
+
+const authRouter = Router();
+
+authRouter.post('/sign-in', validateSignIn, AuthController.signIn);
+authRouter.post('/sign-up', validateSignUp, AuthController.signUp);
+authRouter.post('/refresh', AuthController.updateRefreshToken);
+
+module.exports = authRouter;
