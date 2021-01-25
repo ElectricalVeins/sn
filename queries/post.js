f1755206735e267c6744ef2220a772226754599c
Vladimir <maxmost512@gmail.com>
2021-01-25 11:41:14 +0200

    Add post queryies

---
+++ queries/post.js
@@ -0,0 +1,13 @@
+const { Post, User } = require('../db/models');
+
+module.exports.getAllPosts = ({ offset, limit } = { offset: 0, limit: 50 }) => Post.findAll({
+  include: [{
+    model: User,
+    attributes: ['id', 'firstName', 'lastName', 'imageSrc']
+  }],
+  offset,
+  limit
+});
+
+module.exports.create = (body) => Post.create(body);
+module.exports.update = (values, { id }) => Post.update(values, { where: { id } });
