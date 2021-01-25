3f519e951136ddf4c81ff50774dd122606ddd6ea
Vladimir <maxmost512@gmail.com>
2021-01-25 11:40:48 +0200

    Add post controller

---
+++ routes/post.js
@@ -0,0 +1,24 @@
+const { Router } = require('express');
+const PostController = require('../controllers/post');
+const paginateMW = require('../middlewares/paginate');
+// const { } = require('../middlewares/validation');
+
+const postRouter = Router();
+
+postRouter.route('/')
+  .get(
+    paginateMW,
+    PostController.getAll
+  )
+  .post(
+    // TODO: validation
+    PostController.create
+  );
+
+postRouter.route('/:id')
+  .patch(
+    // TODO: validation
+    PostController.update
+  );
+
+module.exports = postRouter;
